cb325079eb5f9202a23c6c0e30929be93925aef9
FrankDev-327 <salutacinaster@hotmail.com>
2021-07-31 22:22:20 -0500

    make it better

--- models/index.js
+++ models/index.js
@@ -5,7 +5,8 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+//const ff = require('../config/config')
+const config = require('../config/config.js')[env];
 const db = {};
 console.log(config)
 let sequelize;
